099f593a4e11f038f4dc5cec1cb375829a2fd8a6
valentine <yecvhuk.valentine@gmaiil.com>
2023-05-15 11:19:07 +0200

    JS_cicles

--- js/main.js
+++ js/main.js
@@ -1,47 +1,38 @@
 
 // 1. Підрахуйте суму всіх чисел у заданому користувачем діапазоні.
 
-//  {
-//     let num1 = +prompt("Enter first number:");
-//     let num2 = +prompt("Enter second number:");
-//     let min;
-//     let max;
-//     let res = 0;
-//     // if (num1 < num2) {
-//     //     min = num1;
-//     //     max = num2;
-//     // } else if (num1 > num2) {
-//     //     min = num2;
-//     //     max = num1;
-//     // } else {
-//     //     alert("Numbers are equal!");
-//     // }
-//     min = num1 < num2 ? num1 : num2;
-//     max = num1 > num2 ? num1 : num2;
-//     while(min <= max){
-//         res = res + min;
-//         min++;
-//     }
-//     document.write(`<h2> Result :: ${res} </h2> `);
-//  }
+ {
+    let num1 = +prompt("Enter first number:");
+    let num2 = +prompt("Enter second number:");
+    let min;
+    let max;
+    let res = 0;
+    min = num1 < num2 ? num1 : num2;
+    max = num1 > num2 ? num1 : num2;
+    while(min <= max){
+        res = res + min;
+        min++;
+    }
+    document.write(`<p> Result :: <span>${res}</span> </p> `);
+ }
 
 
 
 //  2. Запросіть 2 числа і знайдіть тільки найбільший спільний дільник.
 
-// {
-//     let a = +prompt("Enter");
+{
+    let a = +prompt("Enter");
 
-//     let b = +prompt("Enter");
+    let b = +prompt("Enter");
 
-//     while (b !== 0) {
-//     const temp = b;
-//     b = a % b;
-//     a = temp;
-//     }
+    while (b !== 0) {
+    const temp = b;
+    b = a % b;
+    a = temp;
+    }
 
-//     console.log(a)
-// }
+    document.write(`<p> Greatest common divisor : <span> ${a} </span> </p>`)
+}
 
 
 
@@ -49,70 +40,38 @@
 
 
 
-// {
-//     let number = prompt("Введіть число:");
-//     let divisors = "";
-//     let i = 1;
+{
+    let number = prompt("Enter number:");
+    let divider = "";
+    let i = 1;
 
-//     while (i <= number) {
-//         if (number % i === 0) {
-//             divisors += i +" ";
-//         }
-//         i++;
-//     }
+    while (i <= number) {
+        if (number % i === 0) {
+            divider += i +" ";
+        }
+        i++;
+    }
 
-//     console.log(`Дільники числа ${number}: ${divisors} `);
-// }
-
-
+    document.write(`<p> Divisor ${number}: <span>${divider}</span> </p> `);
+}
 
 
 
-// ///////////////////////////////////////
-// {
-//     let number = +prompt("Enter number");
-//     let counter = 0;
-//     
-//     for(let i = 1; i <= number; i++){
-//         if (number % i == 0) {
-//             // counter++;
-//         }
-//     }
-//     document.write(`${i}`)
-// }
+// 4. Визначте кількість цифр у введеному числі.
 
+{
 
-// {
-//     let number = +prompt("Enter");
-//     let counter = 0;
-//     for(let i = 1; i <= number; i++){
-//         if (number % i == 0) {
-//             counter++;
-//         }
-//     }
-//     if(counter >2){
-//         console.log("Is Prime");
-//     }
-//     else{
-//         console.log("Is Simple");
-//     }
-// }
+    let number = prompt("Enter number:");
+    let count = 0;
+    let n = +number;
 
+    while (n !== 0) {
+    count++;
+    n = parseInt(n / 10);
+    }
 
-// {
-//     // 4. Визначте кількість цифр у введеному числі.
-
-//     let number = prompt("Введіть число:");
-//     let count = 0;
-//     let n = Number(number);
-
-//     while (n !== 0) {
-//     count++;
-//     n = parseInt(n / 10);
-//     }
-
-//     console.log(`Кількість цифр у введеному числі ${number} становить ${count}.`);
-// }
+    document.write(`<p>The number of digits in the entered number  <span> ${number} </span>  is  <span>${count}</span>.</p>`);
+}
 
 
 
@@ -122,80 +81,105 @@
 // статистику на екран. Враховуйте те, що достатньо однієї
 // змінної (не 10) для введення чисел користувачем.
 
-// {
-//     let countPositive = 0;
-//     let countNegative = 0;
-//     let countZero = 0;
-//     let countEven = 0;
-//     let countOdd = 0;
+{
+    let countPositive = 0;
+    let countNegative = 0;
+    let countZero = 0;
+    let even = 0;
+    let odd = 0;
 
-//     for (let i = 1; i <= 10; i++) {
-//         let number = prompt(`Введіть ${i}-е число:`);
-//         let num = parseInt(number);
+    for (let i = 1; i <= 10; i++) {
+        let number = prompt(`Введіть ${i}-е число:`);
+        let num = parseInt(number);
 
-//         if (num > 0) {
-//             countPositive++;
-//         } else if (num < 0) {
-//             countNegative++;
-//         } else {
-//             countZero++;
-//         }
+        if (num > 0) {
+            countPositive++;
+        } else if (num < 0) {
+            countNegative++;
+        } else {
+            countZero++;
+        }
 
-//         if (num % 2 === 0) {
-//             countEven++;
-//         } else {
-//             countOdd++;
-//         }
-//     }
+        if (num % 2 === 0) {
+            even++;
+        } else {
+            odd++;
+        }
+    }
 
-//     console.log(`Кількість додатніх чисел: ${countPositive}`);
-//     console.log(`Кількість від'ємних чисел: ${countNegative}`);
-//     console.log(`Кількість нулів: ${countZero}`);
-//     console.log(`Кількість парних чисел: ${countEven}`);
-//     console.log(`Кількість непарних чисел: ${countOdd}`);
-// }
-
-
+    document.write(`<p>Count Positive: <span>${countPositive}</span> </p>`);
+    document.write(`<p>Count Negative: <span>${countNegative}</span> </p>`);
+    document.write(`<p>Count Zero: <span>${countZero}</span> </p>`);
+    document.write(`<p>Count even: <span>${even}</span> </p>`);
+    document.write(`<p>Count odd: <span>${odd}</span> </p>`);
+}
 
 
-
-///////////////////////////////////////////
 
 // 6. Зацикліть калькулятор. Запросіть у користувача 2 числа і
 // знак, розв’яжіть приклад, виведіть результат і запитайте,
 // чи хоче він розв’язати ще один приклад. І так триватиме
 // доти, доки користувач не відмовиться.
 
-// let repeat = true;
+{
+  let repeat = true;
 
-// while (repeat) {
-//   let num1 = prompt("Введіть перше число:");
-//   let num2 = prompt("Введіть друге число:");
-//   let operator = prompt("Введіть операцію (+, -, *, /)");
+  while (repeat) {
+    let num1 = prompt("Enter the first number:");
+    let num2 = prompt("Enter the second number:");
+    let operator = prompt("Enter the operation (+, -, *, /)");
 
-//   let result;
-//   if (operator === "+") {
-//     result = +num1 + +num2;
-//   } else if (operator === "-") {
-//     result = num1 - num2;
-//   } else if (operator === "*") {
-//     result = num1 * num2;
-//   } else if (operator === "/") {
-//     result = num1 / num2;
-//   } else {
-//     alert("Некоректний оператор");
-//     continue;
-//   }
+    let result;
+    if (operator === "+") {
+      result = +num1 + +num2;
+    } else if (operator === "-") {
+      result = num1 - num2;
+    } else if (operator === "*") {
+      result = num1 * num2;
+    } else if (operator === "/") {
+      result = num1 / num2;
+    } else {
+      alert("Error");
+      continue;
+    }
 
-//   alert(`Результат: ${result}`);
+    alert(`Result :: ${result}`);
 
-//   let answer = prompt("Бажаєте розв'язати ще один приклад? (Так/Ні)").toLowerCase();
-//   if (answer !== "так") {
-//     repeat = false;
-//   }
-// }
+    let answer = prompt("Do you want to solve another example? (Yes/No)");
+    if (answer !== "yes" && answer !== "Yes") {
+      repeat = false;
+    }
+  }
+  alert("Thank you for using the calculator");
+}
 
-// alert("Дякуємо за використання калькулятора!");
+
+// 7. Запросіть у користувача число і на скільки цифр його змі-
+// стити. Змістіть цифри числа та виведіть результат (якщо
+// число 123456 змістити на 2 цифри, то вийде 345612).
+
+{
+  let step = 0;
+  let number = parseInt(prompt("Enter number","123456"));
+  let divide = 10;
+  let tmp = number;
+  while(tmp > 0){
+    step++;
+    tmp = parseInt(tmp / 10);
+  }
+  divide = divide ** (step-1);
+  let offset = +prompt("Enter offset digit");
+  
+  document.write(`<p class="num">${number} </p><br>`);
+
+  for(let i =0; i < offset % step; i++)
+  {
+    let n = parseInt(number / divide);
+    number %= divide;
+    number = number * 10 + n
+  }
+  document.write(`<p class="num">${number}</p> `);
+}
 
 
 
@@ -204,30 +188,33 @@
 // і так доти, доки користувач натискатиме OK.
 
 
-let day = 'понеділок';
-let answer;
+{
+  let day = 'Monday';
+  let answer;
 
-do {
-  console.log(`День тижня: ${day}. Бажаєте побачити назву наступного дня тижня?`);
-  answer = confirm('Бажаєте побачити назву наступного дня тижня?');
-  
-  // Змінюємо день тижня
-  if (day === 'понеділок') {
-    day = 'вівторок';
-  } else if (day === 'вівторок') {
-    day = 'середа';
-  } else if (day === 'середа') {
-    day = 'четвер';
-  } else if (day === 'четвер') {
-    day = 'пʼятниця';
-  } else if (day === 'пʼятниця') {
-    day = 'субота';
-  } else if (day === 'субота') {
-    day = 'неділя';
-  } else if (day === 'неділя') {
-    day = 'понеділок';
-  }
+  do {
+    console.log(`Weekday : ${day}. Want to see the name of the next day of the week?`);
+    answer = confirm('Click OK to continue');
+    
+    
+    if (day === 'Monday') {
+      day = 'Tuesday';
+    } else if (day === 'Tuesday') {
+      day = 'Wednesday';
+    } else if (day === 'Wednesday') {
+      day = 'Thursday';
+    } else if (day === 'Thursday') {
+      day = 'Friday';
+    } else if (day === 'Friday') {
+      day = 'Saturday';
+    } else if (day === 'Saturday') {
+      day = 'Sunday';
+    } else if (day === 'Sunday') {
+      day = 'Monday';
+    }
 } while (answer);
+
+}
 
 
 
@@ -235,31 +222,67 @@ do {
 // 9. Виведіть таблицю множення для всіх чисел від 2 до 9.
 // Кожне число необхідно помножити на числа від 1 до 10.
 
+{
+  document.write("<table>");
+  for(let q = 0; q < 10; q+=5){
+    document.write("<tr>");
+    for(let j = 1; j < 6; j++){
+      document.write("<td>");
+      for( let i = 1; i <= 10; i++){
+        document.write(`${j + q} x ${i} = ${(j + q) * i} <br>`);  
+      }
+      document.write("</td>");
+    }
+    document.write("</tr>");
+    }
+    
+    document.write("</table>");
+}
 
-// document.write("<table>")
-// for(let q = 0; q < 8; q+=4){
-//     document.write("<tr>")
-//     for(let j = 2; j < 6; j++){
-//         document.write("<td>")
-//         for(let i = 1; i <= 10; i++){
-//             document.write(`${j + q} x ${i} = ${(j + q) * i}<br>`);
-//         }
-//         document.write("</td>")
-
-//     }
-//     document.write("</tr>")
-// }
-
-// document.write("</table>")
 
 
-// document.write(`2 x 1 = ${2*1}<br>`);
-// document.write(`2 x 2 = ${2*2}<br>`);
-// document.write(`2 x 3 = ${2*3}<br>`);
-// document.write(`2 x 4 = ${2*4}<br>`);
-// document.write(`2 x 5 = ${2*5}<br>`);
-// document.write(`2 x 6 = ${2*6}<br>`);
-// document.write(`2 x 7 = ${2*7}<br>`);
-// document.write(`2 x 8 = ${2*8}<br>`);
-// document.write(`2 x 9 = ${2*9}<br>`);
-// document.write(`2 x 10 = ${2*10}<br>`);
+// 10. Гра «Вгадай число». Запропонуйте користувачеві загадати
+// число від 0 до 100 і відгадати його наступним способом: в
+// кожній ітерації циклу поділяєте діапазон чисел навпіл,
+// записуєте результат в N і запитуєте у користувача «Ваше
+// число > N, < N або == N?». Залежно від відповіді користу-
+// вача, зменшуєте діапазон. Початковий діапазон від 0 до
+// 100, поділяєте навпіл і отримуєте 50. Якщо користувач
+// вказав, що його число > 50, змінюєте діапазон числа від 51
+// до 100. І так доти, доки користувач не вибере == N.
+
+
+{
+  alert("Gues the number (0 - 100");
+  let counter = 0;
+  let end = 100;
+  let number = end/2;
+  let step = number;
+  while(true){
+    let answer = prompt(`Your number \n> ${number}; \n< ${number}; \n= ${number}`);
+    if(answer == "="){
+      alert(`Your number ${number}`);
+      break;
+    }
+    if(counter > 5){
+      alert("Error!!! You missed your number");
+      break;
+    }
+    step = parseInt(step / 2);
+    if(answer == "<"){
+      number =number - step;
+      if(step == 0){
+        number --;
+        counter ++;
+      }
+    }
+    else{
+      number = number + step;
+      if(step == 0){
+        number ++;
+        counter ++;
+      }
+    }
+  }
+  
+}
